d483b3a751afd80e1aa3d8aed6374a913eea02e1
Gustavo Cardoso <gcardoso1905@gmail.com>
2024-03-13 20:10:19 -0300

    Updates

---
+++ precache-manifest.1af839a26010b1c1d7ea039adde10838.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e334ecbb1b5749912c24a1da27f8122",
+    "url": "/web-fg/index.html"
+  },
+  {
+    "revision": "6e71b56a25f5ad15cd6c",
+    "url": "/web-fg/static/js/2.6785b6e2.chunk.js"
+  },
+  {
+    "revision": "603c3ac71dd938ec6e00",
+    "url": "/web-fg/static/js/main.9d216522.chunk.js"
+  },
+  {
+    "revision": "f713a8c957a6556d0c3c",
+    "url": "/web-fg/static/js/runtime~main.0f70cc33.js"
+  },
+  {
+    "revision": "be41137c64f59e717cc3a4f5823089c2",
+    "url": "/web-fg/static/media/fastgain_logo_F.be41137c.jpeg"
+  }
+]);
